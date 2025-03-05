562db426c067fd5765bf68916c7bf3685c254c4a
William H. Grover <wgrover@gmail.com>
2025-03-05 15:16:56 -0800

    push before reinstalling home mac

---
+++ set.py
@@ -0,0 +1,6 @@
+import shutil
+
+shutil.copy("/Applications/Arduino IDE.app/Contents/Resources/app/lib/backend/resources/arduino-serial-plotter-webapp/static/js/main.35ae02cb.chunk.js",
+            "/Applications/Arduino IDE.app/Contents/Resources/app/lib/backend/resources/arduino-serial-plotter-webapp/static/js/main.35ae02cb.chunk.js.BACKUP")
+f = open("/Applications/Arduino IDE.app/Contents/Resources/app/lib/backend/resources/arduino-serial-plotter-webapp/static/js/main.35ae02cb.chunk.js", "r")
+print(f.read())
